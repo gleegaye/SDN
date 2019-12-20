ff41b0e07fa76c213206419c12014ee4f56d1e36
gleegaye <32540036+gleegaye@users.noreply.github.com>
2019-12-20 16:07:39 +0100

    Update vlan.py

--- vlan.py
+++ vlan.py
@@ -1,121 +1,133 @@
 #!/usr/bin/env python
-
+from mininet.topo import Topo
 from mininet.cli import CLI
-
 from mininet.link import Link,TCLink,Intf
-
 from mininet.net import Mininet
-
 from mininet.node import RemoteController
 
- 
 
-if '__main__' == __name__:
 
-  net = Mininet(link=TCLink)
+class Tree( Topo ):
+	
+	def __init__(self):
+		"creating custom topo"
 
-  h1 = net.addHost('h1')
+		"initialze topo"
+		Topo.__init__( self )
 
-  h2 = net.addHost('h2')
+		#Add hosts 
+		h1 = self.addHost( 'h1' )
+		h2 = self.addHost( 'h2' )
+		h3 = self.addHost( 'h3' )
+		h4 = self.addHost( 'h4' )
+		h5 = self.addHost( 'h5' )
 
-  h3 = net.addHost('h3')
+	
+		#Add switches 
+		s1 = self.addSwitch( 's1' )
+		s2 = self.addSwitch( 's2' )
+		s3 = self.addSwitch( 's3' )
+		s4 = self.addSwitch( 's4' )
+		s5 = self.addSwitch( 's5' )
+		s6 = self.addSwitch( 's6' )
+		s7 = self.addSwitch( 's7' )
+		s8 = self.addSwitch( 's8' )
+		s9 = self.addSwitch( 's9' )
+		s10 = self.addSwitch( 's10' )
 
-  h4 = net.addHost('h4')
+		#Add link for s1
+		self.addLink( s1, s3 )
+		self.addLink( s1, s4 )
+		self.addLink( s1, s5 )
+		self.addLink( s1, s6 )
 
-  #h5 is a switch
+		#Add link for s2
+		self.addLink( s2, s3 )
+		self.addLink( s2, s4 )
+		self.addLink( s2, s5 )
+		self.addLink( s2, s6 )
+		
+		#Add link for s3
+		self.addLink( s3, s7 )
+		self.addLink( s3, s8 )
+	
+		#Add link for s4
+		self.addLink( s4, s7 )
+		self.addLink( s4, s8 )
 
-  h5 = net.addHost('h5')
+		#Add link for s5
+		self.addLink( s5, s9 )
+		self.addLink( s5, s10 )
 
-  #h6 is a router
+		#Add link for s6
+		self.addLink( s6, s9 )
+		self.addLink( s6, s10 )
 
-  h6 = net.addHost('h6')
+		#Add link for s7
+		self.addLink( h1, s7 )
+		self.addLink( h2, s7 )
 
-  Link(h1, h5)
+		#Add link for s8
+		self.addLink( h3, s8 )
 
-  Link(h2, h5)
+		#Add link for s9
+		self.addLink( h4, s9 )
 
-  Link(h3, h5)
+		#Add link for s10
+		self.addLink( h5, s10 )
 
-  Link(h4, h5)
 
-  Link(h5, h6)
+class VLANHost( Host ):
+    "Host connected to VLAN interface"
 
-  net.build()
+    def config( self, vlan=100, **params ):
+        """Configure VLANHost according to (optional) parameters:
+           vlan: VLAN ID for default interface"""
 
-  h1.cmd("ifconfig h1-eth0 0")
+        r = super( VLANHost, self ).config( **params )
 
-  h2.cmd("ifconfig h2-eth0 0")
+        intf = self.defaultIntf()
+        # remove IP from default, "physical" interface
+        self.cmd( 'ifconfig %s inet 0' % intf )
+        # create VLAN interface
+        self.cmd( 'vconfig add %s %d' % ( intf, vlan ) )
+        # assign the host's IP to the VLAN interface
+        self.cmd( 'ifconfig %s.%d inet %s' % ( intf, vlan, params['ip'] ) )
+        # update the intf name and host's intf map
+        newName = '%s.%d' % ( intf, vlan )
+        # update the (Mininet) interface to refer to VLAN interface name
+        intf.name = newName
+        # add VLAN interface to host's name to intf map
+        self.nameToIntf[ newName ] = intf
 
-  h3.cmd("ifconfig h3-eth0 0")
+        return r
 
-  h4.cmd("ifconfig h4-eth0 0")
+hosts = { 'vlan': VLANHost }
 
-  h5.cmd("ifconfig h5-eth0 0")
+class VLANStarTopo( Topo ):
+    """Example topology that uses host in multiple VLANs
+       The topology has a single switch. There are k VLANs with
+       n hosts in each, all connected to the single switch. There
+       are also n hosts that are not in any VLAN, also connected to
+       the switch."""
 
-  h5.cmd("ifconfig h5-eth1 0")
+    def build( self, k=2, n=2, vlanBase=100 ):
+        s1 = self.addSwitch( 's1' )
+        for i in range( k ):
+            vlan = vlanBase + i
+            for j in range(n):
+                name = 'h%d-%d' % ( j+1, vlan )
+                h = self.addHost( name, cls=VLANHost, vlan=vlan )
+                self.addLink( h, s1 )
+        for j in range( n ):
+            h = self.addHost( 'h%d' % (j+1) )
+            self.addLink( h, s1 )
 
-  h5.cmd("ifconfig h5-eth2 0")
 
-  h5.cmd("ifconfig h5-eth3 0")
+def exampleCustomTags():
+    """Simple example that exercises VLANStarTopo"""
 
-  h5.cmd("ifconfig h5-eth4 0")
-
-  h6.cmd("ifconfig h6-eth0 0")
-
-  h5.cmd("vconfig add h5-eth4 10")
-
-  h5.cmd("vconfig add h5-eth4 20")
-
-  h6.cmd("vconfig add h6-eth0 10")
-
-  h6.cmd("vconfig add h6-eth0 20")
-
-  h5.cmd("ifconfig h5-eth4.10 up")
-
-  h5.cmd("ifconfig h5-eth4.20 up")
-
-  h5.cmd("brctl addbr brvlan10")
-
-  h5.cmd("brctl addbr brvlan20")
-
-  h5.cmd("brctl addif brvlan10 h5-eth0")
-
-  h5.cmd("brctl addif brvlan10 h5-eth1")
-
-  h5.cmd("brctl addif brvlan10 h5-eth4.10")
-
-  h5.cmd("brctl addif brvlan20 h5-eth2")
-
-  h5.cmd("brctl addif brvlan20 h5-eth3")
-
-  h5.cmd("brctl addif brvlan20 h5-eth4.20")
-
-  h5.cmd("ifconfig brvlan10 up")
-
-  h5.cmd("ifconfig brvlan20 up")
-
-  h6.cmd("echo 1 > /proc/sys/net/ipv4/ip_forward")
-
-  h6.cmd("ifconfig h6-eth0.10 10.0.10.254 netmask 255.255.255.0")
-
-  h6.cmd("ifconfig h6-eth0.20 10.0.20.254 netmask 255.255.255.0")
-
-  h1.cmd("ifconfig h1-eth0 10.0.10.1 netmask 255.255.255.0")
-
-  h2.cmd("ifconfig h2-eth0 10.0.10.2 netmask 255.255.255.0")
-
-  h3.cmd("ifconfig h3-eth0 10.0.20.1 netmask 255.255.255.0")
-
-  h4.cmd("ifconfig h4-eth0 10.0.20.2 netmask 255.255.255.0")
-
-  h1.cmd("ip route add default via 10.0.10.254 dev h1-eth0")
-
-  h2.cmd("ip route add default via 10.0.10.254 dev h2-eth0")
-
-  h3.cmd("ip route add default via 10.0.20.254 dev h3-eth0")
-
-  h4.cmd("ip route add default via 10.0.20.254 dev h4-eth0")
-
-  CLI(net)
-
-  net.stop()
+    net = Mininet( topo=VLANStarTopo() )
+    net.start()
+    CLI( net )
+    net.stop()
